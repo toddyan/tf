0360387ebec9163537558f711397829faac53813
toddyan <yxdcsu@foxmail.com>
2018-05-21 08:50:07 +0800

    add tf demos

--- queue/batching.py
+++ queue/batching.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 files = tf.train.match_filenames_once("/tmp/examples-*")
-file_queue = tf.train.string_input_producer(string_tensor=files, shuffle=False)
+file_queue = tf.train.string_input_producer(string_tensor=files, shuffle=True)
 reader = tf.TFRecordReader()
 _, serialized_example = reader.read(file_queue)
 example = tf.parse_single_example(serialized_example,features={
@@ -10,13 +10,14 @@ example = tf.parse_single_example(serialized_example,features={
 x, y = example["i"], example["j"]
 batch_size = 3
 capacity = 1000 + batch_size * 3
-x_batch, y_batch = tf.train.batch([x, y], batch_size=batch_size, capacity=capacity)
+#x_batch, y_batch = tf.train.batch([x, y], batch_size=batch_size, capacity=capacity, num_threads=3)
+x_batch, y_batch = tf.train.shuffle_batch([x,y],batch_size=3,capacity=capacity,min_after_dequeue=capacity/2)
 with tf.Session() as s:
     tf.local_variables_initializer().run()
     tf.global_variables_initializer().run()
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=s, coord=coord)
-    for i in range(5):
+    for i in range(2):
         print(s.run([x_batch, y_batch]))
     coord.request_stop()
     coord.join(threads)
