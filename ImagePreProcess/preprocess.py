7c8510dc652f1707030209434a3b584aecc0c39b
toddyan <yxdcsu@foxmail.com>
2018-05-22 08:42:53 +0800

    add tf demos

--- ImagePreProcess/preprocess.py
+++ ImagePreProcess/preprocess.py
@@ -21,21 +21,24 @@ def preprocess_for_train(image, height, weight, bbox, enable_hir_flip):
         image = tf.image.convert_image_dtype(image, tf.float32)
     bbox_begin, bbox_size, _ = tf.image.sample_distorted_bounding_box(image_size=tf.shape(image), bounding_boxes=bbox)
     distorted_image = tf.slice(image, bbox_begin, bbox_size)
-    distorted_image = tf.image.resize_images(distorted_image, size=(height, weight), method=np.random.randint(low=0,high=4))
+    distorted_image = tf.image.resize_images(
+        distorted_image,
+        size=(height, weight),
+        method=np.random.randint(low=0,high=4)
+    )
     if enable_hir_flip == True:
         distorted_image = tf.image.random_flip_left_right(distorted_image)
     distorted_image = distort(distorted_image, np.random.randint(low=0,high=2))
     return distorted_image
 
-image_raw = tf.gfile.FastGFile("/Users/yxd/Downloads/flower_photos/roses/12240303_80d87f77a3_n.jpg",'r').read()
-with tf.Session() as s:
-    image = tf.image.decode_jpeg(image_raw)
-    boxex = tf.constant([[[0.05,0.05,0.9,0.7],[0.2,0.2,0.8,0.86]]])
-    for i in range(5):
-        result = preprocess_for_train(image,299,299,boxex,True)
-        result = result.eval()
-        print(result.shape)
-        plt.imshow(result)
-        plt.show()
-
-
+if __name__ == "__main__":
+    image_raw = tf.gfile.FastGFile("/Users/yxd/Downloads/flower_photos/roses/12240303_80d87f77a3_n.jpg",'r').read()
+    with tf.Session() as s:
+        image = tf.image.decode_jpeg(image_raw)
+        boxex = tf.constant([[[0.05,0.05,0.9,0.7],[0.2,0.2,0.8,0.86]]])
+        for i in range(5):
+            result = preprocess_for_train(image,299,299,boxex,True)
+            result = result.eval()
+            print(result.shape)
+            plt.imshow(result)
+            plt.show()
---
+++ queue/input-precess-framework.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+sys.path.append("../ImagePreProcess")
+import preprocess
+sys.path.append("../LeNet5")
+from comm import ConvLayer
+import infer
+class Conf:
+    def __init__(self):
+        # structure
+        self.image_shape  = (28,28,3)
+        self.classes      = 10
+        self.conv_layers  = [ConvLayer((5,5,3,8), (1,1,1,1), (1,2,2,1), (1,2,2,1)),
+                             ConvLayer((5,5,8,16),(1,1,1,1),(1,2,2,1),(1,2,2,1))]
+        self.fc_layers    = [128, self.classes]
+
+in_path = "/tmp/mnist_tfrecord"
+target_height = 28
+target_weight = 28
+batch_size = 64
+min_after_dequeue = 1000
+capacity = min_after_dequeue + 3 * batch_size
+learning_rate = 0.00001
+
+files = tf.train.match_filenames_once(in_path)
+file_queue = tf.train.string_input_producer(string_tensor=files, shuffle=False)
+reader = tf.TFRecordReader()
+_, serialized_example = reader.read(file_queue)
+example = tf.parse_single_example(
+    serialized_example,
+    features={
+        "image": tf.FixedLenFeature([], tf.string),
+        "label": tf.FixedLenFeature([], tf.int64),
+        "weight": tf.FixedLenFeature([], tf.int64),
+        "height": tf.FixedLenFeature([], tf.int64),
+        "channel": tf.FixedLenFeature([], tf.int64)
+    }
+)
+image,label = example["image"], example["label"]
+height, weight, channel = example["height"], example["weight"], example["channel"]
+image_shape = tf.stack([height, weight, channel])
+image = tf.decode_raw(image,tf.uint8)
+image = tf.reshape(image, shape=image_shape)
+image = tf.image.convert_image_dtype(image, tf.float32)
+
+image = tf.image.grayscale_to_rgb(image)
+'''
+image = preprocess.preprocess_for_train(
+    image,
+    target_height,
+    target_weight,
+    bbox=None,
+    enable_hir_flip=False
+)
+'''
+image = tf.image.resize_images(image, size=[target_height,target_weight])
+
+image_batch, label_batch = tf.train.shuffle_batch(
+    [image, label],
+    batch_size=batch_size,
+    capacity=capacity,
+    min_after_dequeue=min_after_dequeue
+)
+
+logits = infer.infer(Conf(), image_batch, True, None)
+
+cross_entropy = tf.reduce_mean(
+    #tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,labels=label_batch)
+    tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=label_batch)
+)
+
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(logits,1), label_batch)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as s:
+    s.run([tf.global_variables_initializer(),tf.local_variables_initializer()])
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=s, coord=coord)
+    #ib, lb = s.run([image_batch, label_batch])
+    for i in range(100000):
+        s.run(optimizer)
+        if i % 100 == 0 :
+            acc = s.run(accuracy)
+            print(acc)
+    coord.request_stop()
+    coord.join(threads)
