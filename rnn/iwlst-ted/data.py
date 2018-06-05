1a95fbfd5208abe04e61515b1e23a888381fdbe7
toddyan <yxdcsu@foxmail.com>
2018-06-05 21:43:17 +0800

    coding

--- rnn/iwlst-ted/data.py
+++ rnn/iwlst-ted/data.py
@@ -1,3 +1,32 @@
-import tensorflow as tf
+import codecs
+import collections
+from operator import itemgetter
 
 # https://wit3.fbk.eu/mt.php?release=2015-01
+# https://github.com/moses-smt/mosesdecoder/blob/master/scripts/tokenizer/tokenizer.perl
+# git clone -n https://github.com/moses-smt/mosesdecoder.git --depth 1
+# git checkout HEAD scripts
+# ./mosesdecoder/scripts/tokenizer/tokenizer.perl -no-escape -l en <train.tags.en-zh.en >train.txt.en
+# sed 's/ //g; s/\B/ /g; s/[,.?;:\'"!@#$%^&*\(\)_+=\[\]{}<>-]/ /g; s/，/ ， /g; s/。/ 。 /g; s/？/ ？ /g; s/：/ ： /g; s/“/ “ /g; s/”/ ” /g; s/（/ （ /g; s/）/ ） /g ' ./train.tags.en-zh.zh >train.txt.zh
+
+DATA_ROOT="E:/Download/en-zh/"
+
+sep_datas = [DATA_ROOT+"train.txt.en",DATA_ROOT+"train.txt.zh"]
+vocab_datas = [DATA_ROOT+"train.vocab.en",DATA_ROOT+"train.vocab.zh"]
+for (data_file,vocab_file) in zip(sep_datas, vocab_datas):
+    print(data_file,vocab_file)
+    counter = collections.Counter()
+    with codecs.open(data_file, 'r', 'utf-8') as f:
+        for line in f:
+            for word in line.strip().split():
+                counter[word] += 1
+    sorted_word = sorted(counter.items(),key=itemgetter(1),reverse=True)
+    sorted_word = [e[0] for e in sorted_word]
+    sorted_word = ['<sos>','<eos>'] + sorted_word
+    print(len(sorted_word))
+    print(sorted_word[0],sorted_word[-1])
+    if len(sorted_word) > 10000:
+        sorted_word = sorted_word[:10000]
+    with codecs.open(vocab_file, 'w', 'utf-8') as f:
+        for word in sorted_word:
+            f.write(word + "\n")
