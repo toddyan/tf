11270930c0a920106daa78b14f8f840061547123
toddyan <toddyan@tencent.com>
2018-06-17 16:26:42 +0800

    code x

--- globalconf.py
+++ globalconf.py
@@ -2,8 +2,8 @@ import platform
 def get_root():
     if platform.system() == "Windows":
         return "D:/tfroot/"
-    elif platform.system() == "":
-        return ""
+    elif platform.system() == "Linux":
+        return "~/tfroot/"
     return ""
 
 print(platform.system())
